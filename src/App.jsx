6fa2dc2a6a5ad57abddabbfa054d78bd00d22d05
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-06-29 02:35:24 +0200

    clear

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+    import Home from "./home"
+    import Model from "./modal"
+    import SideBar from "./sidebar"
+    
+    const App = ()=>{
+      return <>
+        <Home />
+        <Model />
+        <SideBar />
+      </>
+    }
+    export default App
